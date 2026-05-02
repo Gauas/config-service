f9b92070b546b0736b4665560c590bd8c34f7a27
tnqbao <quocbao.job106240@gmail.com>
2026-05-02 23:09:09 +0700

    refactor: internal middleware

--- middlewares/internal.go
+++ middlewares/internal.go
@@ -8,10 +8,10 @@ import (
 	"github.com/labstack/echo/v4"
 )
 
-func (m *Middleware) Auth() echo.MiddlewareFunc {
+func (m *Middleware) Internal() echo.MiddlewareFunc {
 	return func(next echo.HandlerFunc) echo.HandlerFunc {
 		return func(c echo.Context) error {
-			if subtle.ConstantTimeCompare([]byte(c.Request().Header.Get("secret_key")), []byte(m.secretKey)) != 1 {
+			if subtle.ConstantTimeCompare([]byte(c.Request().Header.Get("Secret-Key")), []byte(m.secretKey)) != 1 {
 				return response.NewError(http.StatusUnauthorized, "unauthorized")
 			}
 			return next(c)
